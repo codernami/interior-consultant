5c55c07f4b5c10311c194399cfc827311e290ea1
codernami <codernami@gmail.com>
2023-03-01 18:10:16 -0300

    Change file location to scripts

--- script.js
+++
@@ -1,7 +0,0 @@
-const iconHamburger = document.getElementById('icon-hamburguer');
-const navMenu = document.getElementById('nav-menu');
-
-iconHamburger.addEventListener("click", e => {
-    iconHamburger.classList.toggle('active')
-    navMenu.classList.toggle('menu__show')
-})
